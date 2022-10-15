7fc39adfebbf97ea1b17ddc67e11a8fb6e672b47
admin <timvillanueva3@gmail.com>
2022-10-15 16:29:18 -0500

    boiler plate and first go at css

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 
+
+let button = document.querySelector("#searchButton")
+
+
+// const url =``
+function addItem(element){
+    let ol = document.querySelector(".list");
+    let li = document.createElement("li");
+    li.setAttribute('id',element.name);
+    li.appendChild(document.createTextNode(`${element.name}`));
+    ol.appendChild(li);
+}
+
+async function getData(event) {
+    
+    let textInput = document.querySelector("#inputBar").value.toLowerCase();
+    event.preventDefault();
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
+    .then( response => {
+        return response.json();
+    })
+    .then(response => {
+
+    })
+    .catch(error => {
+        console.log("failure", error);
+    })
+}
+
+button.addEventListener("click", getData); 
+
+
+
+
