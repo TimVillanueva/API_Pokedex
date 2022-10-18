2d5b0b0b1eb5a939c574a188045e9e9e8c93c27f
admin <timvillanueva3@gmail.com>
2022-10-17 20:17:24 -0500

    national dex API call

--- script.js
+++ script.js
@@ -18,25 +18,76 @@ let grid1 = document.querySelector("#grid1");
 let selector = document.querySelector(".selector");
 
 //Global Variables
-
+let counter=0;
+let nationalDex = false;
 //arrow selector variables
 let position = [0,1,2];
 let currentPosition = position[0];
 //move arrow right  and left
+
+
+//http://pokeapi.co/api/v2/pokemon/?limit=811
+
+// const url =``
+function addItem(element){
+    let nationalDexList = document.querySelector(".nationalDexList");
+    let li = document.createElement("li");
+    li.setAttribute('id',element.name);
+    li.setAttribute('role', "option");
+    li.appendChild(document.createTextNode(`#${counter}:${element.name}`));
+    nationalDexList.appendChild(li);
+    counter++;
+}
+
+async function getData(event) {
+    
+    let textInput = document.querySelector("#inputBar").value.toLowerCase();
+    
+    event.preventDefault();
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
+    .then( response => {
+        return response.json();
+    })
+    .then(response => {
+        let bottomScreen = document.querySelector(".bottomScreen");
+        bottomScreen.innerText = `\n ${response.name} \n id: ${response.id}`;
+        document.querySelector("#inputBar").value = "";
+    })
+    .catch(error => {
+        console.log("failure", error);
+    })
+    
+}
+async function showNationalDex(event) {
+    fetch(`http://pokeapi.co/api/v2/pokemon/?limit=905`)
+    .then( response => {
+        return response.json();
+    })
+    .then(response => {
+        let nationalDexArr = response.results;
+        for (let i=0; i<nationalDexArr.length; i++){
+            addItem(nationalDexArr[i]);
+        }
+    })
+    .catch(error => {
+        console.log("failure", error);
+    })
+}
+
 rightArrow.addEventListener("click", () => {
     if (currentPosition === position[0]){
         selector.style.transform = "translateX(95px)";
         currentPosition = position[1];
     }
     else if (currentPosition === position[1]){
-        selector.style.transform = "translateX(220px)";
+        selector.style.transform = "translateX(210px)";
         currentPosition = position[2];
     }
     if (currentPosition === position[2]){
-        selector.style.transform = "translateX(220px)";
+        selector.style.transform = "translateX(210px)";
         currentPosition = position[2];
     }
-    console.log(currentPosition)
     return currentPosition;
 });
 leftArrow.addEventListener("click", () => {
@@ -55,38 +106,38 @@ leftArrow.addEventListener("click", () => {
     }
     return currentPosition;
 });
-
-
-
-
-// const url =``
-// function addItem(element){
-//     let ol = document.querySelector(".list");
-//     let li = document.createElement("li");
-//     li.setAttribute('id',element.name);
-//     li.appendChild(document.createTextNode(`${element.name}`));
-//     ol.appendChild(li);
-// }
-
-// async function getData(event) {
+enterButton.addEventListener("click", getData); 
+selectButton.addEventListener("click",() => {
+    if (currentPosition === 1){
+        document.querySelector(".searchButton").style.visibility = "hidden";
+        document.querySelector(".nationalDex").style.visibility = "hidden";
+        document.querySelector("#inputBar").style.visibility = "hidden";
+        document.querySelector(".regionalDex").style.visibility = "hidden";
+        document.querySelector(".selector").style.visibility = "hidden";
+        document.querySelector("#rightViewScreen").style.overflowY = "scroll";
+        selector.style.transform = "translateX(0px)";
+        currentPosition = position[0];
+        if (nationalDex === false){
+        showNationalDex();
+        nationalDex=true;
+        }
+        else if (nationalDex === true)
+        {
+        document.querySelector(".nationalDexList").style.visibility = "visible"; 
+        }
+        
+    }
+});
+returnButton.addEventListener("click",() => {
+    document.querySelector("#rightViewScreen").style.overflowY = "hidden";
+    document.querySelector(".searchButton").style.visibility = "visible";
+    document.querySelector(".nationalDex").style.visibility = "visible";
+    document.querySelector("#inputBar").style.visibility = "visible";
+    document.querySelector(".regionalDex").style.visibility = "visible";
+    document.querySelector(".nationalDexList").style.visibility = "hidden";
+    document.querySelector(".selector").style.visibility = "visible";
     
-//     let textInput = document.querySelector("#inputBar").value.toLowerCase();
-//     event.preventDefault();
-
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
-//     .then( response => {
-//         return response.json();
-//     })
-//     .then(response => {
-
-//     })
-//     .catch(error => {
-//         console.log("failure", error);
-//     })
-// }
-
-// button.addEventListener("click", getData); 
-
+});
 
 
 
