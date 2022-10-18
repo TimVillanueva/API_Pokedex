f3c95dc20f94f50464db666a41e59de793fb52df
admin <timvillanueva3@gmail.com>
2022-10-18 16:17:21 -0500

    stats display

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let padUp = document.querySelector("#verticalTop");
 let padDown = document.querySelector("#verticalBottom");
 let padLeft = document.querySelector("#horizontalLeft");
 let padRight = document.querySelector("#horizontalRight");
+let aButton = document.querySelector(".leftButton");
 
 //buttons for right side
 let selectButton = document.querySelector("#whiteSquare1");
@@ -20,17 +21,24 @@ let selector = document.querySelector(".selector");
 
 //Global Variables
 let counter=1;
+let currentPokemon = [];
 let nationalDex = false;
 let regionArr = ['Kanto','Johto','Hoenn','Sinnoh','Unova','Kalos','Alola','Galar'];
-//positional variables
+//positional variables right screen
+let leftSelector = document.querySelector("#leftScreenSelector");
 let position = [0,1,2, 3];
 let currentPosition = position[0];
 let verticalPosition = [0,1,2,3,4,5,6,7];
 let currentVertical = verticalPosition[0];
 let verticalHeight = 20;
+//positional variables left screen
+let leftPosition = [0,1,2];
+let currentLeftPosition = leftPosition[0];
+
 
 //http://pokeapi.co/api/v2/pokemon/?limit=811
 
+//functions
 
 function addItem(element, choice){
     let nationalDexList = document.querySelector(".nationalDexList");
@@ -69,8 +77,8 @@ async function getData(event) {
         let bottomScreen = document.querySelector(".bottomScreen");
         bottomScreen.innerText = `\n ${response.name} \n id: ${response.id}`;
         document.querySelector("#inputBar").value = "";
-        console.log(response);
-        displayInfo(response);
+        currentPokemon = displayInfo(response);
+        return currentPokemon;
     })
     .catch(error => {
         console.log("failure", error);
@@ -82,6 +90,9 @@ const displayInfo = (masterData) => {
     console.log(normalSprite);
     document.querySelector(".sprite").src = normalSprite;
     document.querySelector(".sprite").style.visibility = "visible";
+    document.querySelector("#leftScreenSelector").style.visibility = "visible"
+    document.querySelector(".stats").style.visibility = "visible"
+    document.querySelector(".examine").style.visibility = "visible"
 
     document.querySelector(".displayType").style.visibility = "visible";
     if (masterData.types.length === 1)
@@ -95,7 +106,35 @@ const displayInfo = (masterData) => {
         let type2 = masterData.types[1].type.name
         document.querySelector(".displayType").innerText = `Type: \n ${type1} / ${type2}`
     }
-    
+    return masterData;
+}
+const getStats = (thisPokemon) => {
+    let statValues = []; 
+    for (let i=0; i<6;i++){
+        statValues.push(thisPokemon.stats[i].base_stat)
+
+    }
+    return statValues;
+}
+const displayStats = (statArr) => {
+    hideLeftScreen();
+    document.querySelector(".sprite").style.visibility = "visible";
+    document.querySelector(".sprite").style.height = "250px";
+    document.querySelector(".sprite").style.width = "250px";
+    document.querySelector(".sprite").style.transform = "translateX(-45px)";
+    document.querySelector(".statsDisplay").style.visibility = "visible";
+    document.querySelector(".statsDisplay").innerText =
+    `HP: ${statArr[0]}\n 
+    Attack: ${statArr[1]}\n 
+    Defense: ${statArr[2]}\n 
+    Special Attack: ${statArr[3]}\n 
+    Special Defense: ${statArr[4]}\n 
+    Speed: ${statArr[5]}
+    \n
+    Press A to Return`
+    currentLeftPosition = leftPosition[2];
+
+    return currentLeftPosition;
 }
 async function showNationalDex(event) {
     fetch(`http://pokeapi.co/api/v2/pokemon/?limit=905`)
@@ -135,7 +174,6 @@ const showRegionlDexList = () =>
     document.querySelector(".selector").style.visibility = "visible";
     selector.style.transform = "translate(0px,20px)";
 }
-
 const showHomeScreen =() =>{
     document.querySelector(".regionList").style.visibility="hidden";
     document.querySelector("#rightViewScreen").style.overflowY = "hidden";
@@ -148,6 +186,14 @@ const showHomeScreen =() =>{
     document.querySelector(".selector").style.visibility = "visible";
     
 }
+const hideLeftScreen = () => {
+    document.querySelector(".examine").style.visibility = "hidden";
+    document.querySelector(".displayType").style.visibility = "hidden";
+    document.querySelector("#leftScreenSelector").style.visibility = "hidden";
+    document.querySelector(".stats").style.visibility = "hidden";
+    document.querySelector(".sprite").style.visibility = "hidden";
+    document.querySelector(".statsDisplay").style.visibility = "hidden";
+}
 const showRegionList = (position) => {
     
     document.querySelector("#regionalDexList").style.visibility = "hidden";
@@ -155,7 +201,7 @@ const showRegionList = (position) => {
     console.log(position)
     createRegionalDex(position);
 }
-
+//event Listeners
 rightArrow.addEventListener("click", () => {
     if (currentPosition === position[0]){
         selector.style.transform = "translateX(78px)";
@@ -211,6 +257,22 @@ upArrow.addEventListener("click", () => {
 }
 })
 enterButton.addEventListener("click", getData); 
+aButton.addEventListener("click", () => {
+    if (currentLeftPosition === 0)
+    {
+        let statValueArr = getStats(currentPokemon);
+        displayStats(statValueArr);
+    }
+    else if (currentLeftPosition === 2)
+    {
+        hideLeftScreen();
+        document.querySelector(".sprite").style.height = "150px";
+        document.querySelector(".sprite").style.width = "150px";
+        document.querySelector(".sprite").style.transform = "translateX(0px)";
+        displayInfo(currentPokemon);
+        currentLeftPosition = leftPosition[0];
+    }
+})
 selectButton.addEventListener("click",() => {
     
     if (currentPosition === 1){
@@ -239,6 +301,24 @@ selectButton.addEventListener("click",() => {
         showRegionList(regionArr[currentVertical]);
     }
 });
+padDown.addEventListener("click", () => {
+    if (currentLeftPosition === 0){
+        leftSelector.style.transform = "translateY(30px)";
+        currentLeftPosition = leftPosition[1];
+    }
+    else if (currentLeftPosition === 1){
+        return;
+    }
+})
+padUp.addEventListener("click", () => {
+    if (currentLeftPosition === 0){
+        return;
+    }
+    else if (currentLeftPosition === 1){
+        leftSelector.style.transform = "translateY(0px)";
+        currentLeftPosition = leftPosition[0];
+    }
+})
 returnButton.addEventListener("click",() => {
     if(currentPosition === 3){
     selector.style.transform = "translateX(225px)";
